c71547ff51a15cce60701323268b9440fead97f9
Joseph Phelan <joephela@gmail.com>
2024-11-01 11:35:48 -0500

    Updates

--- assets/index-B7rBgJUy.js
+++ assets/index-B7rBgJUy.js
@@ -426,9 +426,12 @@ object-assign
   display: flex;
   gap: 16px;
   width: 90%;
-  border: 1px solid;
-  border-radius: 4px;
+  border-bottom: 1px solid;
   padding: 16px;
+
+  &&:last-child {
+    border-bottom: none;
+  }
 
   h3 {
     padding: 0;
