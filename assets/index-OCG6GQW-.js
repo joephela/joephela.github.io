011c8c74a90bbc0dcb6646c5227175d8cc62c35a
Joephela <joephela@gmail.com>
2025-02-02 12:54:16 -0600

    Updates

--- assets/index-OCG6GQW-.js
+++ assets/index-OCG6GQW-.js
@@ -130,7 +130,7 @@ Error generating stack: `+o.message+`
 
   height: 200px;
   width: 200px;
-  clip-path: xywh(40px 5px 79% 79% round 25% 5%);
+  clip-path: xywh(40px 5px 79% 79% round 25% 25%);
   transition: clip-path 1s;
   animation: path 15s ease infinite;
 
